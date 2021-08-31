02916a4263d9476479d259a6e3eb8b92d2f24ec1
Chirag <chiragkasat0@gmail.com>
2021-09-01 02:30:56 +0530

    change flename to add-current-user

--- auth-server/src/routes/signout.ts
+++ auth-server/src/routes/signout.ts
@@ -1,9 +1,19 @@
 import { Request, Response, Router } from 'express';
+import { addCurrentUser } from '../middlewares/add-current-user';
+import { redisClient } from '../redisClient';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = Router();
 
-router.post('/signout', (req: Request, res: Response) => {
-	res.json({});
+router.post('/signout', addCurrentUser, (req: Request, res: Response) => {
+	try {
+		req.session = null;
+		if (req.currentUser) redisClient.del(req.currentUser.id.toString());
+		res.json({});
+	} catch (e) {
+		console.log(e);
+		throw new BadRequestError('Something went wrong. Try Again');
+	}
 });
 
 export { router as signoutRouter };
